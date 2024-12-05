c6196394c7fcbc5e4947712b53bda575e72c6104
Patty <alonso.aypat22@triana.salesianos.edu>
2024-12-05 16:53:47 +0100

    faltan cositas

--- prueba_1/lib/login.dart
+++ prueba_1/lib/login.dart
@@ -14,46 +14,72 @@ class _LoginState extends State<Login> {
     return Scaffold(
       appBar: AppBar(
         
-         backgroundColor: Colors.red,
+         backgroundColor: const Color.fromRGBO(230, 47, 22, 1),
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         
       ),
       body: Container(
-        color: Colors.red, 
+        color:  const Color.fromRGBO(230, 47, 22, 1), 
         width: double.infinity,     
         padding: const EdgeInsets.only(right: 10, left: 10, top: 5),  
         child:Column(
           
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Container(
               width: double.infinity,
               height: 300,
-              color: Colors.amber,
-              child: const Row(
+              color: const Color.fromRGBO(230, 47, 22, 1),
+              child: const Column(
                 mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   Image(
                   image: AssetImage('images/path_logo.png'),
                   width: 200,
-                ),
+                  ),
+                  Text('Beutiful, Private Sharing', style: TextStyle(color: Colors.white)),
                 ],
               ),
             ),
             Container(
               width: double.infinity,
-              height: 340,
-              color: Colors.amber,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+              height: 300,
+              
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                   ElevatedButton(
                   onPressed: () {},
-                  child: const Text('Sign up'),
+                  style: ElevatedButton.styleFrom(
+                    fixedSize: Size(220, 50), 
                   
-                ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5),
+                  )
+                  ),
+                  child: const Text('Sign up', style: TextStyle(color:Color.fromRGBO(230, 47, 22, 1), fontSize: 20),),
+                  
+                  ),
+                  Column(
+                    
+                    children: [
+                      const Text('Already have a Path account?'),
+                      OutlinedButton(
+                        onPressed: () {},
+                        child: const Text('Log in'),
+                        style: ElevatedButton.styleFrom(
+                          fixedSize: Size(220, 50), 
+                        
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5),
+                          ),    
+                        )
+                      ),
+                    ],
+                  ),
+                  Text('By using Path, you agree to paths'),
                 ],
               ),
             )
