44081f3f7920d102aa9bc01fbaa44c13d57578b9
Patricia Alonso Ayllón <alonso.aypat22@salesianos.triana.edu>
2024-12-17 12:05:55 +0100

    barra botom terminada

--- filmapi/lib/screen/MainMenuScreen.dart
+++ filmapi/lib/screen/MainMenuScreen.dart
@@ -13,7 +13,25 @@ class _MainmenuscreenState extends State<Mainmenuscreen> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color.fromRGBO(92, 247, 228, 100),
-        title: Text('FILMAPI'),
+        title: Text('FILMAPI', style: TextStyle(fontWeight: FontWeight.w900, color: Color.fromRGBO(29, 41, 72, 1)), textAlign:,),
+      ),
+      
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        showSelectedLabels: true,
+        showUnselectedLabels: true,
+        backgroundColor: Color.fromRGBO(246, 240, 152, 1),
+        items: const<BottomNavigationBarItem>[       
+        BottomNavigationBarItem(icon: Icon(Icons.movie_creation_outlined), label: 'Peliculas'),
+        BottomNavigationBarItem(icon: Icon(Icons.camera_roll_outlined), label: 'Series'),
+        BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: 'Inicio'),
+        BottomNavigationBarItem(icon: Icon(Icons.people_alt_outlined), label: 'Actores'),
+        BottomNavigationBarItem(icon: Icon(Icons.person_outline), label: 'Mi Perfil')
+      ],
+      unselectedItemColor:  Color.fromRGBO(29, 41, 72, 1),
+      unselectedLabelStyle: TextStyle(color: Color.fromRGBO(29, 41, 72, 1)),
+      selectedItemColor: Color.fromRGBO(233, 106, 83, 1),
+      selectedLabelStyle: TextStyle(color: Color.fromRGBO(233, 106, 83, 1)),
       ),
     );
   }
